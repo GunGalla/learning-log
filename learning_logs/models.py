b9f16f8faa3b27e3bc5f925fd81c5d0126d7d0fa
Iaroslav <GunGalla@gmail.com>
2023-01-23 14:16:27 +0200

    Add first class and migrations

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
+"""Models class module."""
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """User's topic."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns model as a string."""
+        return self.text
