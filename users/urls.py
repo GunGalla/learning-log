b35f6cd8d1bf0215fbbbb0fb0c63fcbb3faad1d2
Iaroslav <GunGalla@gmail.com>
2023-01-27 22:05:20 +0200

    Add registration function

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ app_name = 'users'
 urlpatterns = [
     # Default auth URL
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""Views of user's page."""
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Register new user."""
+    if request.method != 'POST':
+        # Shows empty form
+        form = UserCreationForm()
+    else:
+        # Filled form processing
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Login and redirect to homepage
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Show empty or invalid form
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
