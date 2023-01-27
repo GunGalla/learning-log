6ea375e96c6194a9ccc2e861f511015a89bb9371
Iaroslav <GunGalla@gmail.com>
2023-01-27 23:07:05 +0200

    Add topics and users connections

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,11 +1,13 @@
 """Models class module."""
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
     """User's topic."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Returns model as a string."""
