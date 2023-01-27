51b37a50d15229db76408899409ed25b7ec2c27d
Iaroslav <GunGalla@gmail.com>
2023-01-27 18:45:22 +0200

    Entries edit preparation

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # New entry creation page
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Enrty edit page
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
